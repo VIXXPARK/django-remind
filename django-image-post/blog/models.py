b3a23aed150458664bd5f58661fed12de56a982a
Suhan Park <vixx170627@gmail.com>
2021-06-22 20:35:30 +0900

    이미지 복수 처리 확인

---
+++ django-image-post/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Blog(models.Model):
+    title=models.CharField(max_length=25)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Image(models.Model):
+    blogId=models.ForeignKey(Blog,on_delete=models.CASCADE)
+    image=models.ImageField(default='media/제목없음.jpeg',upload_to="")
---
+++ django-image-post/blog/serializers.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Image,Blog
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['image']
+    
+class BlogSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True,read_only=True)
+    class Meta:
+        model = Blog
+        fields= ('id','title','images')
+    
+    def create(self,validated_data):
+        images_data = self.context['request'].FILES
+        blog = Blog.objects.create(**validated_data)
+        for image_data in images_data.getlist('images'):
+            Image.objects.create(blogId=blog,image=image_data)
+        return blog
