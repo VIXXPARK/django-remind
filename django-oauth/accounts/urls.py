acebfffb1a1ec5bda784da5d8787234cc4a7d1c9
VIXXPARK <vixx170627@gmail.com>
2021-11-07 10:42:29 +0900

    주석 추가

--- django-oauth/accounts/urls.py
+++ django-oauth/accounts/urls.py
@@ -3,4 +3,7 @@ from .views import kakao_get_login, get_user_info
 urlpatterns=[
     path('accounts/kakao/login/', kakao_get_login),
     path('user/kakao/callback/', get_user_info, name="kakao_callback"),
+    """
+    두 번째 URL 은 kakao developers redirect url에 추가해주시면 됩니다. 
+    """
 ]
